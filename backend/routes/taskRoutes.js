520a4fd66e1b25477adfcd6e3632c88c7621db03
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2026-01-17 13:33:24 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('../controllers/taskController');
+const taskController = require('..backend/controllers/taskController');
 
 router.get('/tasks', taskController.getTasks);
 router.post('/tasks', taskController.createTask);
